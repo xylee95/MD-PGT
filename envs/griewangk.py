5f9d39fbb143b8f6517bde2f67022ad3fed8e4ea
xylee95 <xylee@iastate.edu>
2021-05-02 11:37:36 -0500

    modified to more stable reward function for discrete setting.

--- envs/griewangk.py
+++ envs/griewangk.py
@@ -12,16 +12,12 @@ class Griewangk(gym.Env):
         self.seed = seed
         np.random.seed(self.seed)
         self.dimension = dimension
-        self.min_action = -0.5
-        self.max_action = 0.5 
         self.max_bound = 25
         self.min_bound = -25
-        self.initial_pos =  np.random.uniform(low=self.min_bound, high=self.max_bound, size=(self.dimension,))
         self.y = 1 #dummy variable
         self.prev_y = 1 #dummy variable
-        self.action_space = spaces.Box(low=self.min_action, high=self.max_action, shape=(self.dimension,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=self.min_action, high=self.max_action, shape=(self.dimension,), dtype=np.float32)
-        self.done = False
+        self.action_space = spaces.Discrete(self.dimension) #3 action per agent (up, down, stay) ^ num agents
+        self.observation_space = spaces.Box(low=self.min_bound, high=self.max_bound, shape=(self.dimension,), dtype=np.float32)
         self.reward = 0
         self.reset()
         # only used for contour plotting purposes for 2D
@@ -30,15 +26,23 @@ class Griewangk(gym.Env):
     def step(self, action):
         #action is vector of delta
         action = action[0].detach().numpy()
-        self.state = self.state + action
+        delta = []
+        for item in action:
+            if item == 0:
+                change = 0.05
+            elif item == 1:
+                change = -0.05
+            elif item == 2:
+                change = 0.00
+            delta.append(change)
+        self.state = self.state + delta
         self.state = np.clip(self.state, self.min_bound, self.max_bound)
         self.prev_y = self.y
         self.y = self.eval_func(action)
         self.reward = self.get_reward()
-        if np.less_equal((np.absolute(self.state)), np.ones(self.dimension)*1e-3).all():
-            self.done = True
-            self.reward = 10
-        # another termintion condition on f(y) maybe?
+        # if np.less_equal((np.absolute(self.state)), np.ones(self.dimension)*1e-3).all():
+        #     self.done = True
+        #     self.reward = 10
         return self.state, self.reward, self.done, self.y
 
     def reset(self):
@@ -50,11 +54,11 @@ class Griewangk(gym.Env):
         # Reward compute based on y: we want previous y to be bigger than current y
         # Might also want to consider based on distance of x from optimal
 
-        #reward = -0.1*self.y + (self.prev_y - self.y)
+        reward = -0.1*self.y + (self.prev_y - self.y)
         #reward = -0.1*self.y + 0.1*(self.prev_y - self.y)
 
         #r3
-        reward = -0.1*self.y 
+        #reward = -0.1*self.y 
 
         #r4
         #reward = -0.1*np.linalg.norm(self.state)
--- envs/sphere.py
+++ envs/sphere.py
@@ -10,38 +10,42 @@ class Sphere(gym.Env):
         self.seed = seed
         np.random.seed(self.seed)
         self.dimension = dimension
-        self.min_action = -0.5
-        self.max_action = 0.5 
-        self.initial_pos =  np.random.uniform(low=-10, high=10, size=(self.dimension,))
         self.max_bound = 10
         self.min_bound = -10
         self.y = 1 #dummy variable
         self.prev_y = 1 #dummy
-        self.action_space = spaces.Box(low=self.min_action, high=self.max_action, shape=(self.dimension,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=self.min_action, high=self.max_action, shape=(self.dimension,), dtype=np.float32)
+        self.action_space = spaces.Discrete(self.dimension) #3 action per agent (up, down, stay) ^ num agents
+        self.observation_space = spaces.Box(low=self.min_bound, high=self.max_bound, shape=(self.dimension,), dtype=np.float32)
         self.done = False
         self.reward = 0
         self.reset()
         # only used for contour plotting purposes for 2D
         self.minima = np.array([0,0])
 
-        
     def step(self, action):
-        #action is vector of delta
+        #action is vector of decision (0: up, 1: down, 2: stay)
         action = action[0].detach().numpy()
-        self.state = self.state + action
+        delta = []
+        for item in action:
+            if item == 0:
+                change = 0.05
+            elif item == 1:
+                change = -0.05
+            elif item == 2:
+                change = 0.00
+            delta.append(change)
+        self.state = self.state + delta
         self.state = np.clip(self.state, self.min_bound, self.max_bound)
         self.prev_y = self.y
         self.y = self.eval_func(action)
         self.reward = self.get_reward()
-        if np.less_equal((np.absolute(self.state)), np.ones(self.dimension)*1e-3).all():
-            self.done = True
-            self.reward = 10
-        # another termintion condition on f(y) maybe?
+        # if np.less_equal((np.absolute(self.state)), np.ones(self.dimension)*1e-3).all():
+        #     self.done = True
+        #     self.reward = 0.1
         return self.state, self.reward, self.done, self.y
 
     def reset(self):
-        self.state =  np.random.uniform(low=self.min_bound, high=self.max_bound, size=(self.dimension,))
+        self.state =  np.floor(np.random.uniform(low=self.min_bound, high=self.max_bound, size=(self.dimension,)))
         self.done = False
         return np.array(self.state)
 
@@ -49,11 +53,11 @@ class Sphere(gym.Env):
         # Reward compute based on y: we want previous y to be bigger than current y
         # Might also want to consider based on distance of x from optimal
 
-        #reward = -0.1*self.y + (self.prev_y - self.y)
+        reward = -0.1*self.y + (self.prev_y - self.y)
         #reward = -0.1*self.y + 0.1*(self.prev_y - self.y)
 
         #r3
-        reward = -0.1*self.y 
+        #reward = -0.1*self.y 
 
         #r4
         #reward = -0.1*np.linalg.norm(self.state)
